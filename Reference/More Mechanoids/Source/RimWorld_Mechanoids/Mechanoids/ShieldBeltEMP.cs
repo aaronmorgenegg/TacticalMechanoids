7acc1474a16cf0d8b63a2d041cdc07c4ccae01a0
aaronmorgenegg <aaronmorgenegg@gmail.com>
2020-08-27 16:25:18 -0600

    Add folder structure and 3 other mods for reference

---
+++ Reference/More Mechanoids/Source/RimWorld_Mechanoids/Mechanoids/ShieldBeltEMP.cs
@@ -0,0 +1,69 @@
+using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace MoreMechanoids
+{
+    [StaticConstructorOnStartup]
+    public class ShieldBeltEMP : ShieldBelt
+    {
+        private static Material bubbleMat;
+        private ReflectionCache<ShieldBelt> reflection;
+        public ShieldBeltEMP()
+        {
+            reflection = new ReflectionCache<ShieldBelt>(this);
+        }
+
+        public override void DrawWornExtras()
+        {
+            if (ShieldState == ShieldState.Active && reflection.GetProperty<bool>("ShouldDisplay"))
+            {
+                float num = Mathf.Lerp(1.2f, 1.55f, reflection.GetField<float>("energy")) * 2.5f; // Added multiplier. The rest is all from base.
+                Vector3 drawPos = Wearer.Drawer.DrawPos;
+                drawPos.y = AltitudeLayer.MoteOverhead.AltitudeFor();
+                int num2 = Find.TickManager.TicksGame - reflection.GetField<int>("lastAbsorbDamageTick");
+                if (num2 < 8)
+                {
+                    float num3 = (8 - num2) / 8f * 0.05f;
+                    drawPos += reflection.GetField<Vector3>("impactAngleVect") * num3;
+                    num -= num3;
+                }
+
+                float angle = Rand.Range(0, 360);
+                Vector3 s = new Vector3(num, 1f, num);
+                Matrix4x4 matrix = default;
+                matrix.SetTRS(drawPos, Quaternion.AngleAxis(angle, Vector3.up), s);
+                if (!bubbleMat) bubbleMat = MaterialPool.MatFrom("Other/ShieldBubble", ShaderDatabase.Transparent, Color.cyan);
+                Graphics.DrawMesh(MeshPool.plane10, matrix, bubbleMat, 0);
+            }
+        }
+
+        public override bool CheckPreAbsorbDamage(DamageInfo dinfo)
+        {
+            if (ShieldState != 0)
+            {
+                return false;
+            }
+
+            if (dinfo.Def == DamageDefOf.EMP)
+            {
+                var newValue = reflection.GetField<float>("energy") - dinfo.Amount * reflection.GetField<float>("EnergyLossPerDamage");
+                reflection.SetField("energy", newValue);
+
+                if (Energy < 0)
+                {
+                    reflection.InvokeMethod<object>("Break");
+                }
+                else
+                {
+                    Traverse.Create(this).Method("AbsorbedDamaged").SetValue(dinfo);
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Reference/More Mechanoids/Source/RimWorld_Mechanoids/Mechanoids/Verb_UnlockDoor.cs
@@ -0,0 +1,99 @@
+using System;
+using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+using Verse.Sound;
+
+namespace MoreMechanoids
+{
+    public class Verb_UnlockDoor : Verb_MeleeAttack
+    {
+        private static SoundDef unlockDoorSound;
+
+        protected override bool TryCastShot()
+        {
+            if (!CasterPawn.Spawned)
+            {
+                return false;
+            }
+            if (CasterPawn.stances.FullBodyBusy)
+            {
+                return false;
+            }
+            //Log.Message("Trying open door on "+currentTarget.Thing);
+            if(!(currentTarget.Thing is Building_Door door)) return false;
+            if (door.Open)
+            {
+                CasterPawn.jobs.StopAll();
+                return true;
+            }
+            if (!CanHitTarget(door))
+            {
+                Log.Warning(string.Concat(CasterPawn, " unlocked ", door, " from out of position."));
+                return false;
+            }
+            CasterPawn.rotationTracker.Face(door.DrawPos);
+
+            var nonMissChance = Traverse.Create(this).Method("GetNonMissChance", (LocalTargetInfo)door).GetValue<float>();
+            // Wood door ~100%, steel (160hp) ~69%, granite (270hp) ~41%, plasteel (450hp) ~24%
+            var hpChance = 110.0f / Math.Max(door.HitPoints, 1);
+            var unlockChance = Math.Min(nonMissChance, hpChance);
+            
+            if(Rand.Chance(unlockChance))
+            {
+                UnlockDoor(door);
+            }
+            else
+            {
+                var soundMiss = Traverse.Create(this).Method("SoundMiss").GetValue<SoundDef>();
+                soundMiss.PlayOneShot(new TargetInfo(door.Position, door.MapHeld)); 
+                CreateCombatLog(maneuver => maneuver.combatLogRulesMiss, false);
+            }
+            CasterPawn.Drawer.Notify_MeleeAttackOn(door);
+            CasterPawn.rotationTracker.FaceCell(door.Position);
+            CasterPawn.caller?.Notify_DidMeleeAttack();
+            return true;
+        }
+
+        private void UnlockDoor(Building_Door door)
+        {
+            if (door.def.defName == "HeronInvisibleDoor")
+            {
+                ForceDoorExtended(door);
+            }
+            else
+            {
+                ForceDoor(door);
+            }
+
+            if (unlockDoorSound == null) unlockDoorSound = SoundDef.Named("Explosion_EMP");
+
+            unlockDoorSound.PlayOneShot(SoundInfo.InMap(CasterPawn));
+
+            Vector3 loc = door.Position.ToVector3ShiftedWithAltitude(1);
+            MoteMaker.ThrowMicroSparks(loc, door.Map);
+            MoteMaker.ThrowLightningGlow(loc, door.Map, Rand.Range(0.7f, 1.5f));
+
+            CasterPawn.jobs.StopAll();
+        }
+
+        private static void ForceDoor(Building_Door door)
+        {
+            Traverse.Create(door).Method("DoorOpen", 60).GetValue();
+            Traverse.Create(door).Field("holdOpenInt").SetValue(true);
+        }
+
+        private static void ForceDoorExtended(Building_Door door)
+        {
+            var building = Traverse.Create(door).Field("parentDoor").GetValue<Building>();
+            Traverse.Create(building).Method("DoorOpen", 60).GetValue();
+            Traverse.Create(building).Field("holdOpenInt").SetValue(true);
+        }
+
+        protected override DamageWorker.DamageResult ApplyMeleeDamageToTarget(LocalTargetInfo target)
+        {
+            return new DamageWorker.DamageResult();
+        }
+    }
+}
