00583aa6dff73601cd8c7fee4ba189a4f3e9bedd
aaronmorgenegg <aaronmorgenegg@gmail.com>
2020-08-28 15:52:14 -0600

    Scoping out Generate Mechanites function

---
+++ Source/TacticalMechanoids/TacticalMechanoids/CompProperties/CompProperties_GenerateMechanites.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+using Verse;
+
+namespace TacticalMechanoids
+{
+    public class CompProperties_GenerateMechanites : CompProperties
+    {
+        private int mechaniteSpawnRate = 500;
+        private float nonMechanoidChance = 0.1f;
+        private HediffDef MechanoidMechanitesHediff = HediffDefOf.ConstructiveMechanites;
+        private HediffDef NonMechanoidMechanitesHediff = HediffDefOf.FibrousMechanites;
+
+        public CompProperties_GenerateMechanites()
+        {
+            this.compClass = typeof(CompGenerateMechanites);
+        }
+    }
+}
---
+++ Source/TacticalMechanoids/TacticalMechanoids/ThingComps/ThingComp_GenerateMechanites.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+using Verse;
+
+namespace TacticalMechanoids
+{
+    public class GenerateMechanites : ThingDef
+    {
+        private int tickCounter = 0;
+
+        public CompProperties_GenerateMechanites Props
+        {
+            get
+            {
+                return (CompProperties_GenerateMechanites)this.props;
+            }
+        }
+
+        public override Tick()
+        {
+            base.Tick();
+            tickCounter++;
+
+            // TODO: If statement to check if pawn has a working TM_MechaniteSource, compare current tickCounter to mechaniteSpawnRate adjusted by TM_MechaniteSource effectiveness.
+            if (pawn.TM_MechaniteSource && tickCounter >= mechaniteSpawnRate * TM_MechaniteSource)
+            {
+                if (pawn.health != null)
+                {
+                    // TODO: for pawn in getNextClosestPawn:
+                        // TODO: If pawn is friendly mechanoid, 
+                            // TODO: If pawn does not have MechanoidMechanitesHediff:
+                                // TODO: Apply MechanoidMechanitesHediff hediff and break.
+                        // TODO: elif pawn is enemy humanoid
+                            // TODO: if random.randFloat(0, 1) < nonMechanoidChance and pawn does not have NonMechanoidMechanitesHediff:
+                                // TODO: Apply NonMechanoidMechanitesHediff and break.
+                }
+                tickCounter = 0;
+            }
+        }
+    }
+}
