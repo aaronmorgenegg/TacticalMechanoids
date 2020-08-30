cc61779aff4b88a6993d5e6438dbbc8bd51da1f1
aaronmorgenegg <aaronmorgenegg@gmail.com>
2020-08-29 18:04:33 -0600

    More Bug Fixes

---
+++ Source/TacticalMechanoids/TacticalMechanoids/Hediffs/HediffCompProperties_ConstructiveMechanites.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+using Verse;
+
+namespace TacticalMechanoids
+{
+    public class HediffCompProperties_ConstructiveMechanites : HediffCompProperties
+    {
+        public float constructiveMechaniteHealStrength = 0.1f;
+        public float constructiveMechaniteHealRate = 30;
+        public float constructiveMechaniteDuration = 1000;
+
+        public HediffCompProperties_ConstructiveMechanites()
+        {
+            this.compClass = typeof(HediffCompConstructiveMechanites);
+        }
+    }
+}
---
+++ Source/TacticalMechanoids/TacticalMechanoids/Hediffs/HediffComp_ConstructiveMechanites.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+
+using RimWorld;
+using Verse;
+
+namespace TacticalMechanoids
+{
+    public class HediffCompConstructiveMechanites : HediffComp
+    {
+        private int healCounter = 0;
+        private int durationCounter = 0;
+
+        public HediffCompProperties_ConstructiveMechanites Props
+        {
+            get
+            {
+                return (HediffCompProperties_ConstructiveMechanites)this.props;
+            }
+        }
+
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            base.CompPostTick(ref severityAdjustment);
+            healCounter++;
+            //durationCounter++;
+
+            if (healCounter >= this.Props.constructiveMechaniteHealRate)
+            {
+                if (this.parent.pawn.health != null)
+                {
+                    if (this.parent.pawn.health.hediffSet.GetInjuriesTendable() != null && this.parent.pawn.health.hediffSet.GetInjuriesTendable().Count<Hediff_Injury>() > 0)
+                    {
+                        foreach (Hediff_Injury injury in this.parent.pawn.health.hediffSet.GetInjuriesTendable())
+                        {
+                            injury.Severity -= this.Props.constructiveMechaniteHealStrength;
+                            break;
+                        }
+                    }
+                }
+                healCounter = 0; // TODO: Constructive MEchanites has comps, needs to inherit from HediffWithcomps or get rid of comps?
+            }
+
+            /*if (durationCounter >= this.Props.constructiveMechaniteDuration)
+            {
+                TODO: May not need this.
+                TODO: End hediff effect.
+            }*/
+        }
+    }
+}
