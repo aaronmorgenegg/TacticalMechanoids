69bdb71828f868b13c61291ba86c1b55c662cedd
aaronmorgenegg <aaronmorgenegg@gmail.com>
2020-08-31 17:12:16 -0600

    Change toxic sludge hediff to apply burns and blisters

---
+++ Source/TacticalMechanoids/TacticalMechanoids/Hediffs/HediffComp_ToxicSludge.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+using RimWorld;
+using Verse;
+
+namespace TacticalMechanoids
+{
+    public class HediffCompConstructiveMechanites : HediffWithComps
+    {
+        private int healCounter = 0;
+        private float constructiveMechaniteHealStrength = 0.1f;
+        private float constructiveMechaniteHealRate = 60;
+
+        public override void Tick()
+        {
+            base.Tick();
+            healCounter++;
+
+            if (healCounter >= constructiveMechaniteHealRate)
+            {
+                if (pawn.health != null)
+                {
+                    if (pawn.health.hediffSet.GetInjuriesTendable() != null && pawn.health.hediffSet.GetInjuriesTendable().Count<Hediff_Injury>() > 0)
+                    {
+                        foreach (Hediff_Injury injury in pawn.health.hediffSet.GetInjuriesTendable())
+                        {
+                            injury.Severity -= constructiveMechaniteHealStrength;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
