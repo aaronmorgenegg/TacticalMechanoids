282ae23bc782a6be6eb9e52bb5fe4ffaa1facbc1
aaronmorgenegg <aaronmorgenegg@gmail.com>
2020-08-31 18:19:43 -0600

    Update code to use BurnsOverTime comp

--- Source/TacticalMechanoids/TacticalMechanoids/Hediffs/HediffComp_ToxicSludge.cs
+++ Source/TacticalMechanoids/TacticalMechanoids/Hediffs/HediffComp_ToxicSludge.cs
@@ -7,52 +7,48 @@ using Verse;
 
 namespace TacticalMechanoids
 {
-    public class HediffComp_ToxicSludge : HediffWithComps
+    public class HediffComp_BurnsOverTime : HediffComp
     {
         private int burnCounter = 0;
-        private int burnRate = 9000;
-        private float burnVariance = 0.25f;
-        private float currentBurnVariance = 0f;
-        private int burnDamage = 5;
-        private float burnDamageVariance = 0.5f;
         private int blisterCounter = 0;
-        private int blisterRate = 15000;
-        private float blisterVariance = 0.25f;
+        private float currentBurnVariance = 0f;
         private float currentBlisterVariance = 0f;
 
-        public override void Tick()
+        public HediffCompProperties_BurnsOverTime Props => (HediffCompProperties_BurnsOverTime)props;
+
+        public override void CompPostTick(ref float severityAdjustment)
         {
-            base.Tick();
+            base.CompPostTick(ref severityAdjustment);
             burnCounter++;
             blisterCounter++;
 
-            float burnTime = (burnRate* (1 + currentBurnVariance)) / (1 + Severity);
+            float burnTime = (Props.burnRate* (1 + currentBurnVariance)) / (1 + parent.Severity);
             if (burnCounter >= burnTime)
             {
                 burnCounter = 0;
-                currentBurnVariance = Rand.Range(-burnVariance, burnVariance);
-                if (pawn.health != null)
+                currentBurnVariance = Rand.Range(-Props.burnVariance, Props.burnVariance);
+                if (Pawn.health != null)
                 {
-                    pawn.TakeDamage(new DamageInfo(DefDatabase<DamageDef>.GetNamed("Burn"), (burnDamage * Rand.Range(-burnDamageVariance, burnDamageVariance)), 0f, -1f, null, null, null, DamageInfo.SourceCategory.ThingOrUnknown, null));
+                    Pawn.TakeDamage(new DamageInfo(DefDatabase<DamageDef>.GetNamed("Burn"), (Props.burnDamage * Rand.Range(-Props.burnDamageVariance, Props.burnDamageVariance)), 0f, -1f, null, null, null, DamageInfo.SourceCategory.ThingOrUnknown, null));
                 }
             }
 
-            float blisterTime = (blisterRate * (1 + currentBlisterVariance)) / (1 + Severity);
+            float blisterTime = (Props.blisterRate * (1 + currentBlisterVariance)) / (1 + parent.Severity);
             if (blisterCounter >= blisterTime)
             {
-                if (pawn.health != null)
+                if (Pawn.health != null)
                 {
                     blisterCounter = 0;
-                    currentBlisterVariance = Rand.Range(-blisterVariance, blisterVariance);
-                    IEnumerable<BodyPartRecord> injuredBodyParts = pawn.health.hediffSet.GetInjuredParts();
+                    currentBlisterVariance = Rand.Range(-Props.blisterVariance, Props.blisterVariance);
+                    IEnumerable<BodyPartRecord> injuredBodyParts = Pawn.health.hediffSet.GetInjuredParts();
                     HediffDef burnHediff = HediffDef.Named("Burn");
                     // TODO: Randomize list first?
                     foreach (BodyPartRecord bodyPart in injuredBodyParts)
                     {
-                        if (pawn.health.hediffSet.HasHediff(burnHediff, bodyPart))
+                        if (Pawn.health.hediffSet.HasHediff(burnHediff, bodyPart))
                         {
-                            pawn.health.AddHediff(HediffDef.Named("TM_Blister"), bodyPart, null);
-                            string message = "TM_DevelopedBlister".Translate(pawn.Label);
+                            Pawn.health.AddHediff(HediffDef.Named("TM_Blister"), bodyPart, null);
+                            string message = "TM_DevelopedBlister".Translate(Pawn.Label);
                             Messages.Message(message, MessageTypeDefOf.NegativeEvent);
                             break;
                         }
