60d07367c174544dbb41d9b560c167fa12cc70d8
aaronmorgenegg <aaronmorgenegg@gmail.com>
2020-08-31 18:19:19 -0600

    Refactor ToxicSludge to use a new BurnsOverTime comp

---
+++ Source/TacticalMechanoids/TacticalMechanoids/Hediffs/HediffCompProperties_BurnsOverTime.cs
@@ -0,0 +1,20 @@
+﻿using RimWorld;
+using Verse;
+
+namespace TacticalMechanoids
+{
+    public class HediffCompProperties_BurnsOverTime : HediffCompProperties
+    {
+        public int burnRate = 9000;
+        public float burnVariance = 0.25f;
+        public int burnDamage = 5;
+        public float burnDamageVariance = 0.5f;
+        public int blisterRate = 15000;
+        public float blisterVariance = 0.25f;
+
+        public HediffCompProperties_BurnsOverTime()
+        {
+            this.compClass = typeof(HediffComp_BurnsOverTime);
+        }
+    }
+}
